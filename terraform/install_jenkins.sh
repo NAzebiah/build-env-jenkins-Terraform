2512a1ad6f04f22473a1a936cd37c7184cf82822
Noel Azebiah <Noel.Azebiah@scouts.org.uk>
2020-01-25 23:44:02 +0000

    first commit

---
+++ terraform/install_jenkins.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+sudo yum -y update
+
+echo "Install Java JDK 8"
+yum remove -y java
+yum install -y java-1.8.0-openjdk
+
+echo "Install Maven"
+
+yum install -y maven 
+
+echo "Install git"
+yum install -y git
+
+echo "Install Docker engine"
+yum update -y
+yum install docker -y
+#sudo usermod -a -G docker jenkins
+#sudo service docker start
+sudo chkconfig docker on
+
+echo "Install Jenkins"
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+yum install -y jenkins
+sudo usermod -a -G docker jenkins
+sudo chkconfig jenkins on
+sudo service docker start
+#sudo service jenkins start
+
+echo "Setup SSH key"
+mkdir /var/lib/jenkins/.ssh
+touch /var/lib/jenkins/.ssh/known_hosts
+chown -R jenkins:jenkins /var/lib/jenkins/.ssh
+chmod 700 /var/lib/jenkins/.ssh
+mv /tmp/id_rsa /var/lib/jenkins/.ssh/id_rsa
+chmod 600 /var/lib/jenkins/.ssh/id_rsa
+
+echo "Configure Jenkins"
+mkdir -p /var/lib/jenkins/init.groovy.d
+mv /tmp/basic-security.groovy /var/lib/jenkins/init.groovy.d/basic-security.groovy
+mv /tmp/disable-cli.groovy /var/lib/jenkins/init.groovy.d/disable-cli.groovy
+mv /tmp/csrf-protection.groovy /var/lib/jenkins/init.groovy.d/csrf-protection.groovy
+mv /tmp/disable-jnlp.groovy /var/lib/jenkins/init.groovy.d/disable-jnlp.groovy
+mv /tmp/jenkins.install.UpgradeWizard.state /var/lib/jenkins/jenkins.install.UpgradeWizard.state
+mv /tmp/node-agent.groovy /var/lib/jenkins/init.groovy.d/node-agent.groovy
+chown -R jenkins:jenkins /var/lib/jenkins/jenkins.install.UpgradeWizard.state
+mv /tmp/jenkins /etc/sysconfig/jenkins
+chmod +x /tmp/install-plugins.sh
+bash /tmp/install-plugins.sh
+sudo service jenkins start
---
+++ terraform/main.tf
@@ -0,0 +1,7 @@
+
+provider "aws" {
+  region                  = "${var.region}"
+  # shared_credentials_file = "C:\Users\noela\.aws"
+  # profile                 = "development"
+}
+
