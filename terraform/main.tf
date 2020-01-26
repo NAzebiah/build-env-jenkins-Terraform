aa20c36549640098d7fa4e70a701ce62fee8d90f
NAzebiah <Noel.Azebiah@nfinancial.co.uk>
2020-01-26 05:17:46 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 
 provider "aws" {
   region                  = "${var.region}"
-  # shared_credentials_file = "C:\Users\noela\.aws"
+  # shared_credentials_file = "xxxx"
   # profile                 = "development"
 }
 
